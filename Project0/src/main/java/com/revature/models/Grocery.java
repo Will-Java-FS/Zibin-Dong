e1f9dd71e2f2f4f1de1c15f51bbfaed79b73fc20
ZBDGIT <zbd.github@gmail.com>
2024-07-31 22:56:42 -0700

    Update Grocery model

--- Project0/src/main/java/com/revature/models/Grocery.java
+++ Project0/src/main/java/com/revature/models/Grocery.java
@@ -1,4 +1,90 @@
 package com.revature.models;
 
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Groceries")
 public class Grocery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @Column(columnDefinition = "int CHECK (quantity>=0)")
+    private int quantity;
+
+    @Column(columnDefinition = "double CHECK (price>0)")
+    private double price;
+
+    public Grocery() {
+    }
+
+    public Grocery(int id, String name, int quantity, double price)
+    {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Grocery grocery = (Grocery) o;
+        return getId() == grocery.getId() && getQuantity() == grocery.getQuantity() && Double.compare(getPrice(), grocery.getPrice()) == 0 && Objects.equals(getName(), grocery.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getQuantity(), getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Grocery{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
 }
