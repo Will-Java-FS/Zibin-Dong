fe56e5076e53ef06059a026e35f980ad619901af
gufe2213 <gufe2213@colorado.edu>
2024-08-01 14:28:59 -0600

    Grocery controller

--- Project0/src/main/java/com/revature/controllers/GroceryController.java
+++ Project0/src/main/java/com/revature/controllers/GroceryController.java
@@ -1,13 +1,55 @@
 package com.revature.controllers;
 
+import com.revature.models.Grocery;
+import com.revature.services.GroceryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/groceries")
+@CrossOrigin
 public class GroceryController {
-    //Create new item
+    GroceryService gs;
+
+    @Autowired
+    public GroceryController(GroceryService gs) {this.gs = gs;}
 
     //View all items
+    @GetMapping
+    public List<Grocery> getAllGroceries() {return gs.getAllGroceries();}
 
     //View item by id
+    @GetMapping("/{id}")
+    public Grocery getGrocery() {return gs.getGrocery(id);}
+
+    //Create new item
+    @PostMapping(consumes = "application/json", produces ="application/json")
+    public ResponseEntity<Grocery> addGrocery(@RequestBody Grocery g) {
+        g = gs.addGrocery(g);
+        return new ResponseEntity<>(g, HttpStatus.OK);
+    }
 
     //Update item
+    @PutMapping("/{id}")
+    public ResponseEntity<Grocery> updateGrocery(@PathVariable int id, @RequestBody Grocery g) {
+        g.setId(id);
+        Grocery gOld = gs.getActor(id);
+        if (gOld.getId() == id) {
+            g = gs.updateGrocery(g);
+            return new ResponseEntity<>(g, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+    }
 
     //Delete item by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Grocery> deleteGrocery(@PathVariable int id) {
+        return gs.deleteGrocery(id);
+    }
+
 }
