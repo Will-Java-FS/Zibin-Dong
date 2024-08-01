6288951e520062cdc5ce1908c9af2bfe4686aae8
ZBDGIT <zbd.github@gmail.com>
2024-08-01 14:25:40 -0700

    little change

--- Project0/src/main/java/com/revature/repositories/GroceryRepo.java
+++ Project0/src/main/java/com/revature/repositories/GroceryRepo.java
@@ -3,6 +3,8 @@ package com.revature.repositories;
 
 import com.revature.models.Grocery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroceryRepo extends JpaRepository<Grocery, Integer> {
 }
--- Project0/src/main/java/com/revature/services/AccountService.java
+++ Project0/src/main/java/com/revature/services/AccountService.java
@@ -1,4 +1,10 @@
 package com.revature.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+    @Autowired
+    public
 }
