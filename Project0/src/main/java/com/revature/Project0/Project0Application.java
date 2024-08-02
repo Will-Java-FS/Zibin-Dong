f098ab5bf85eb3acb52d9251a2bfd2fa3492ad9a
gufe2213 <gufe2213@colorado.edu>
2024-08-02 13:25:16 -0600

    Update base app class

--- Project0/src/main/java/com/revature/Project0/Project0Application.java
+++ Project0/src/main/java/com/revature/Project0/Project0Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.revature")
 @EntityScan("com.revature.models")
 @EnableJpaRepositories("com.revature.repositories")
 public class Project0Application {
