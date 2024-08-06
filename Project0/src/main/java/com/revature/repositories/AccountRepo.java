e8dd00b6fdc9bee31be2c2bfa616e79e48da2d81
gufe2213 <gufe2213@colorado.edu>
2024-08-06 10:05:27 -0600

    Removed unused imports

--- Project0/src/main/java/com/revature/repositories/AccountRepo.java
+++ Project0/src/main/java/com/revature/repositories/AccountRepo.java
@@ -1,13 +1,10 @@
 package com.revature.repositories;
 
 import com.revature.models.Account;
-import com.revature.models.Grocery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface AccountRepo extends JpaRepository<Account, Integer> {
